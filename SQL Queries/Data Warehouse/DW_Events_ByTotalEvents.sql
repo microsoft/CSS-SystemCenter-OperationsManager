c02b09d98a400daa2db49d7aea32fe4358fae367
Blake Drumm <blakedrumm@microsoft.com>
2024-05-08 11:47:08 -0400

    Change to Rendered Description :cake:

--- SQL Queries/Data Warehouse/DW_Events_ByTotalEvents.sql
+++ SQL Queries/Data Warehouse/DW_Events_ByTotalEvents.sql
@@ -16,7 +16,7 @@
 -- Selects the top 100 records from the result set
 SELECT TOP 100
     evt.EventDisplayNumber,            -- Display number of the event
-    evtd.RawDescription,               -- Raw description of the event
+    evtd.RenderedDescription,               -- Raw description of the event
     evtlc.ComputerName,                -- Name of the computer logging the event
     COUNT(*) AS TotalEvents,           -- Total number of events aggregated by display number, description, and computer name
     DATEDIFF(DAY, MIN(evt.DateTime), MAX(evt.DateTime)) + 1 AS DaysOfData  -- Calculates the span of days between the earliest and latest event dates for each group
@@ -30,7 +30,7 @@ INNER JOIN
     ON evt.LoggingComputerRowId = evtlc.EventLoggingComputerRowId
 GROUP BY 
     evt.EventDisplayNumber,            -- Groups the results by event display number,
-    evtd.RawDescription,               -- raw event description,
+    evtd.RenderedDescription,               -- raw event description,
     evtlc.ComputerName                 -- and computer name
 ORDER BY 
     TotalEvents DESC                   -- Orders the results by the total number of events, in descending order
